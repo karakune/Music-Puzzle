a1a8a9622c2dbaf14060246ada4cb34c7bc791e8
Francis Pellerin <hugopelletier783@gmail.com>
2018-07-21 16:11:42 -0400

    Making prefabs and adding shapes

--- Sound Room/Assets/Scripts/SoundBouncer.cs
+++ Sound Room/Assets/Scripts/SoundBouncer.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class SoundBouncer : MonoBehaviour
 {
     // public Vector3 newRotation;
+    
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Killzone"))
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = initialPosition;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
 }
